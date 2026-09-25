60b9f4961fcb4aee7cf962f898a7a794ebb417e5
vrba-c <deathbladesacc@gmail.com>
2026-09-25 19:32:08 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Caches the app so it opens and runs with no internet connection.
-const CACHE = 'stakeout-v10';
+const CACHE = 'stakeout-v11';
 const SHELL = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)).then(() => self.skipWaiting()));
